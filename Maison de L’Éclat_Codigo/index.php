41880d1993115a310c85b7d0abbba8ce13da256e
mariavitoriajesus <102930406+mariavitoriajesus@users.noreply.github.com>
2025-06-09 17:41:50 -0300

    código

---
+++ Maison de L’Éclat_Codigo/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style_home.css">
+    <title>Maison de L’Éclat</title>
+</head>
+<body>
+
+<header>
+  <div class="header-content">
+    <div class="logo-area">
+      <div class="logo-circle">MAISON DE<br>L’ÉCLAT</div>
+    </div>
+    <nav>
+      <ul class="menu">
+        <li><a href="index.php">Início</a></li>
+        <li><a href="pages/produtos.php">Produtos</a></li>
+        <li><a href="pages/contatos.php">Contato</a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
+
+
+<main>
+    <!-- <div class="banner-overlay">
+      <div class="banner-text-small">
+        Sua beleza, nossa inspiração: <br />
+        Maquiagem clean <br />
+        com toque francês
+      </div>
+      <div class="banner-text-large">
+        SUA BELEZA, NOSSA INSPIRAÇÃO: <br />
+        MAQUIAGEM CLEAN
+      </div>
+    </div>  -->
+    <!-- <div class="logo-circles">
+      <div class="circle big-circle">
+        <span>MAISON DE<br />L’ÉCLAT</span>
+      </div>
+      <div class="circle small-circle">MAISON DE<br />L'ÉCLAT</div>
+    </div> -->
+  </main>
+
+<section class="produtos-section">
+  <div class="produtos-carousel">
+    <div class="card">
+      <img src="img/Makeup Brush and Pastel Powders 1.png" alt="Pincel">
+      <p>Pincel</p>
+    </div>
+    <div class="card">
+    <img src="img/Makeup Brush and Pastel Powders 1.png" alt="Pincel">
+      <p>Pincel</p>
+    </div>
+    <div class="card">
+    <img src="img/Makeup Brush and Pastel Powders 1.png" alt="Pincel">
+      <p>Pincel</p>
+    </div>
+    <div class="card">
+    <img src="img/Makeup Brush and Pastel Powders 1.png" alt="Pincel">
+      <p>Pincel</p>
+    </div>
+  </div>
+
+  <div class="produtos-categoria">
+    <div class="categoria-card">
+      <img src="img/Floral Beauty Serenity Flat Lay 1.png" alt="Cosmetics">
+      <p>Cosmetics</p>
+    </div>
+    <div class="categoria-card">
+    <img src="img/Floral Beauty Serenity Flat Lay 1.png" alt="Cosmetics">
+      <p>Cosmetics</p>
+    </div>
+  </div>
+</section>
+
+
+<footer>
+  © 2025 Maison de L’Éclat – Elegância e beleza com essência francesa
+</footer>
+
+</body>
+</html>
---
+++ Maison de L’Éclat_Codigo/pages/contatos.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/style_home.css">
+    <title>Contatos</title>
+</head>
+<body>
+<header>
+  <div class="header-content">
+    <div class="logo-area">
+      <div class="logo-circle">MAISON DE<br>L’ÉCLAT</div>
+    </div>
+    <nav>
+      <ul class="menu">
+        <li><a href="../index.php">Início</a></li>
+        <li><a href="produtos.php">Produtos</a></li>
+        <li><a href="contatos.php">Contato</a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
+
+
+
+
+<footer>
+  © 2025 Maison de L’Éclat – Elegância e beleza com essência francesa
+</footer>
+</body>
+</html>
